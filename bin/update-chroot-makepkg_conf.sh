f707d59d6f8ced27e32ffefe99f7eacba12b441d
Gábor Zóka <59315013+gabor-zoka@users.noreply.github.com>
2020-10-17 06:48:51 +0100

    Removed a hack by using -Bstatic instead of -static.

--- bin/update-chroot-makepkg_conf.sh
+++ bin/update-chroot-makepkg_conf.sh
@@ -4,6 +4,12 @@ set -e -u -o pipefail
 
 perl -i -p -e 's{!libtool}{libtool}; s{!staticlibs}{staticlibs}' "$1"
 perl -i -p -e 's{^(C(XX)?FLAGS=).*}{$1"-march=sandybridge -O3 -pipe"}' "$1"
-perl -i -p -e 's{^(LDFLAGS=).*}{PKG_CONFIG="pkg-config --static"\n$1"-O1 -static -lm"}' "$1"
+# -static is NOT the same as -Bstatic despite the man page.
+#
+# xz, bzip2, and libvpx all fail if I use -static with a crtbeginT error.
+#
+# As per https://stackoverflow.com/a/31681323, which is seriously underrated 
+# comment, I changed -static to -Bstatic and all worked.
+perl -i -p -e 's{^(LDFLAGS=).*}{PKG_CONFIG="pkg-config --static"\n$1"-O1 -Bstatic -lm"}' "$1"
 perl -i -p -e 's{^\#(MAKEFLAGS=).*}{$1"-j\$\(nproc\)"}' "$1"
 perl -i -p -e 's{^\#(BUILDDIR=.*)}{$1}' "$1"
