30dde6f2d3f46c1648035ae3c4f359b26c2dc430
Gábor Zóka <59315013+gabor-zoka@users.noreply.github.com>
2020-10-17 07:23:15 +0100

    Removed -lm from LDFLAGS as it breaks pkg-config in ffmpeg. Indeed, it is not my job to add it, but the package should add it to *.pc file. All compiles, but libbs2b, which I have fixed locally.
    In addition, I have removed an accidentally added file.

--- bin/update-chroot-makepkg_conf.sh
+++ bin/update-chroot-makepkg_conf.sh
@@ -10,6 +10,6 @@ perl -i -p -e 's{^(C(XX)?FLAGS=).*}{$1"-march=sandybridge -O3 -pipe"}' "$1"
 #
 # As per https://stackoverflow.com/a/31681323, which is seriously underrated 
 # comment, I changed -static to -Bstatic and all worked.
-perl -i -p -e 's{^(LDFLAGS=).*}{PKG_CONFIG="pkg-config --static"\n$1"-O1 -Bstatic -lm"}' "$1"
+perl -i -p -e 's{^(LDFLAGS=).*}{PKG_CONFIG="pkg-config --static"\n$1"-O1 -Bstatic"}' "$1"
 perl -i -p -e 's{^\#(MAKEFLAGS=).*}{$1"-j\$\(nproc\)"}' "$1"
 perl -i -p -e 's{^\#(BUILDDIR=.*)}{$1}' "$1"
