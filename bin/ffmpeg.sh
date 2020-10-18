57469cbb51d895423bfc8f30f7fb3260e7c78970
Gábor Zóka <59315013+gabor-zoka@users.noreply.github.com>
2020-10-18 04:06:38 +0100

    Renamed the ffmpeg pkg to ffmpeg-static.

--- bin/ffmpeg.sh
+++ bin/ffmpeg.sh
@@ -5,7 +5,7 @@ set -e -u -o pipefail
 r=/mnt/gl.sys/chroot
 p=/home/gabor/sw/arch/pkg-static
 
-cd /home/gabor/sw/ffmpeg-static/asp/ffmpeg
+cd /home/gabor/sw/ffmpeg-static/asp/ffmpeg-static
 sudo makechrootpkg -T -c -r $r\
     -I $p/libvorbis-aotuv-*.pkg.tar.zst -I $p/libogg-*.pkg.tar.zst\
     -I $p/zlib-*.pkg.tar.zst\
