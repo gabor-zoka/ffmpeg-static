bcbab4e3dab8772740b2027f5c3a47fb5fb13323
Gábor Zóka <59315013+gabor-zoka@users.noreply.github.com>
2020-10-18 01:49:37 +0100

    Minor bugfixes.

--- bin/ffmpeg.sh
+++ bin/ffmpeg.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
 r=/mnt/gl.sys/chroot
 p=/home/gabor/sw/arch/pkg-static
 
--- bin/libs.sh
+++ bin/libs.sh
@@ -1,4 +1,4 @@
-#1/usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e -u -o pipefail
 
